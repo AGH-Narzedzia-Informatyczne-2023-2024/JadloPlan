2d150e3ca244c1046719b57ca87b7617226f0025
margaj <margaj366@gmail.com>
2023-11-18 00:10:09 +0100

    Podstawowy program do zebrania przepisu od użytkownika, w przyszłości rozwinie się o zapisywanie do bazzy

--- mar.py
+++ mar.py
@@ -1,1 +1,22 @@
+# Funkcja do pobierania liczby porcji
+def pobierz_liczbe_porcji():
+    while True:
+        try:
+            liczba_porcji = int(input("Podaj liczbę porcji: "))
+            return liczba_porcji
+        except ValueError:
+            print("Błąd! Podaj liczbę całkowitą.")
 
+# Pobieranie informacji od użytkownika
+nazwa_przepisu = input("Podaj nazwę przepisu: ")
+skladniki = input("Podaj składniki, oddzielając je przecinkiem: ").split(',')
+przygotowanie = input("Podaj sposób przygotowywania: ")
+liczba_porcji = pobierz_liczbe_porcji()
+
+# Wyświetlanie informacji
+print("\nPrzepis: {}".format(nazwa_przepisu))
+print("\nSkładniki:")
+for skladnik in skladniki:
+    print("- {}".format(skladnik.strip()))  # Usunięcie zbędnych białych znaków
+print("\nSposób przygotowywania: {}".format(przygotowanie))
+print("\nLiczba porcji: {}".format(liczba_porcji))
